0881b0b22c49afaf899f410d850d210f6655cde2
NeoPhoenix7 <133651625+NeoPhoenix7@users.noreply.github.com>
2023-07-07 15:54:06 +0200

    Javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger')
+const close = document.querySelector('.close_menu')
+const nav2 = document.querySelector('.nav2')
+
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.add('hide');
+    close.classList.remove('hide');
+    nav2.classList.toggle('visible')
+})
+
+close.addEventListener('click', ()=> {
+    close.classList.add('hide');
+    hamburger.classList.remove('hide');
+    nav2.classList.toggle('hide')
+})
